96a7ba26829980e991c280f649f90e76dba04e7c
rkdwlss2 <rkdwlss2@gmail.com>
2020-09-26 01:10:45 +0900

    Initial Commit

---
+++ boj1926bfs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  boj1926bfs
+//
+//  Created by 강명진 on 2020/09/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
