3092049c1650e02e9e4270e6bd7fec1231aa67cf
rkdwlss2 <rkdwlss2@gmail.com>
2021-09-10 00:11:21 +0900

    algo

--- boj10868.cpp
+++ boj10868.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-#include <algorithm>
-#define lli long long int
-using namespace std;
-
-lli tree[100001*4];
-lli tree2[100001 * 4];
-lli arr[100005];
-
-lli init(lli node, lli start, lli end) {
-    if (start == end)return tree[node] = arr[start];
-    else return tree[node] = min(init(node * 2, start, (start + end) / 2), init(node * 2 + 1, (start + end) / 2 + 1, end));
-}
-
-lli init2(lli node, lli start, lli end) {
-    if (start == end)return tree2[node] = arr[start];
-    else return tree2[node] = max(init2(node * 2, start, (start + end) / 2), init2(node * 2 + 1, (start + end) / 2 + 1, end));
-}
-
-lli getMin(int node, int st, int en, int l, int r) {
-    if (r<st || l>en)return 10000000009;
-    if (l <= st && en <= r)return tree[node];
-    return min(getMin(node * 2, st, (st + en) / 2, l, r), getMin(node * 2 + 1, (st + en) / 2 + 1, en, l, r));
-}
-
-lli getMax(int node, int st, int en, int l, int r) {
-    if (r<st || l>en)return 0;
-    if (l <= st && en <= r)return tree2[node];
-    return max(getMax(node * 2, st, (st + en) / 2, l, r), getMax(node * 2 + 1, (st + en) / 2 + 1, en, l, r));
-}
-
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        cin >> arr[i];
-    }
-    init(1, 1, n);
-    init2(1, 1, n);
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        cout << getMin(1, 1, n, a, b)  << '\n';
-    }
-}
+//#include <iostream>
+//#include <algorithm>
+//#define lli long long int
+//using namespace std;
+//
+//lli tree[100001*4];
+//lli tree2[100001 * 4];
+//lli arr[100005];
+//
+//lli init(lli node, lli start, lli end) {
+//    if (start == end)return tree[node] = arr[start];
+//    else return tree[node] = min(init(node * 2, start, (start + end) / 2), init(node * 2 + 1, (start + end) / 2 + 1, end));
+//}
+//
+//lli init2(lli node, lli start, lli end) {
+//    if (start == end)return tree2[node] = arr[start];
+//    else return tree2[node] = max(init2(node * 2, start, (start + end) / 2), init2(node * 2 + 1, (start + end) / 2 + 1, end));
+//}
+//
+//lli getMin(int node, int st, int en, int l, int r) {
+//    if (r<st || l>en)return 10000000009;
+//    if (l <= st && en <= r)return tree[node];
+//    return min(getMin(node * 2, st, (st + en) / 2, l, r), getMin(node * 2 + 1, (st + en) / 2 + 1, en, l, r));
+//}
+//
+//lli getMax(int node, int st, int en, int l, int r) {
+//    if (r<st || l>en)return 0;
+//    if (l <= st && en <= r)return tree2[node];
+//    return max(getMax(node * 2, st, (st + en) / 2, l, r), getMax(node * 2 + 1, (st + en) / 2 + 1, en, l, r));
+//}
+//
+//
+//int main() {
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    int n, m;
+//    cin >> n >> m;
+//    for (int i = 1; i <= n; i++) {
+//        cin >> arr[i];
+//    }
+//    init(1, 1, n);
+//    init2(1, 1, n);
+//    for (int i = 0; i < m; i++) {
+//        int a, b;
+//        cin >> a >> b;
+//        cout << getMin(1, 1, n, a, b)  << '\n';
+//    }
+//}
--- boj11003-2.cpp
+++ boj11003-2.cpp
@@ -1,22 +1,22 @@
-#include <cstdio>
-#include <algorithm>
-#include <deque>
-#include <iostream>
-using namespace std;
-int l, n, x;
-deque<pair<int, int>> dq;
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n>>l;
-    for (int i = 1; i <= n; i++) {
-        while (dq.size() && dq.front().second <= i - l)dq.pop_front();
-        cin>>x;
-        while (dq.size() && dq.back().first >= x)
-            dq.pop_back();
-        dq.push_back({ x,i });
-        cout<< dq.front().first<<' ';
-    }
-    return 0;
-}
-
+//#include <cstdio>
+//#include <algorithm>
+//#include <deque>
+//#include <iostream>
+//using namespace std;
+//int l, n, x;
+//deque<pair<int, int>> dq;
+//int main() {
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    cin>>n>>l;
+//    for (int i = 1; i <= n; i++) {
+//        while (dq.size() && dq.front().second <= i - l)dq.pop_front();
+//        cin>>x;
+//        while (dq.size() && dq.back().first >= x)
+//            dq.pop_back();
+//        dq.push_back({ x,i });
+//        cout<< dq.front().first<<' ';
+//    }
+//    return 0;
+//}
+//
