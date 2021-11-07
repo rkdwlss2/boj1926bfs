cc64182bfdc6a366c6e8880dc3ce3815a2901d19
rkdwlss2 <rkdwlss2@gmail.com>
2021-11-07 12:34:37 +0900

    boj11660

---
+++ boj15904.cpp
@@ -0,0 +1,27 @@
+////
+////  boj15904.cpp
+////  boj1926bfs
+////
+////  Created by 강명진 on 2021/09/20.
+////
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//string s="UCPC";
+//
+//int main(){
+//    string str;
+//    getline(cin,str);
+//    int index=0;
+//    for (int i =0;i<str.size();i++){
+//        if (s[index]==str[i])
+//            index++;
+//    }
+//    if (index==4){
+//        cout<<"I love UCPC"<<'\n';
+//    }else{
+//        cout<<"I hate UCPC"<<'\n';
+//    }
+//}
