3010e34f3593eaeea6da6257ce2e20de7bac93ee
rkdwlss2 <rkdwlss2@gmail.com>
2021-11-08 21:51:03 +0900

    boj14920

---
+++ boj1550.cpp
@@ -0,0 +1,27 @@
+////
+////  boj1550.cpp
+////  boj1926bfs
+////
+////  Created by 강명진 on 2021/10/04.
+////
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    string str;
+//    cin>>str;
+//    int sum=0;
+//    for (int i = 0 ; i<str.size();i++){
+//        sum*=16;
+//        if (str[i]>='0'&&str[i]<='9'){
+//            sum+=str[i]-'0';
+//        }else{
+//            sum+=str[i]-'A'+10;
+//        }
+//    }
+//    cout<<sum<<'\n';
+//}
