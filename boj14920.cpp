e26084217bd8191f9c08ff59a54b52db614938ab
rkdwlss2 <rkdwlss2@gmail.com>
2021-11-13 18:50:55 +0900

    boj1213

---
+++ boj14920.cpp
@@ -0,0 +1,27 @@
+////
+////  boj14920.cpp
+////  boj1926bfs
+////
+////  Created by 강명진 on 2021/11/08.
+////
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main(){
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    int c1;
+//    cin>>c1;
+//    int index=1;
+//    while(true){
+//        if (c1==1)break;
+//        if (c1%2==0){
+//            c1=c1/2;
+//        }else{
+//            c1=3*c1+1;
+//        }
+//        index++;
+//    }
+//    cout<<index<<'\n';
+//}
