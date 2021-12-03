c301ac95c2f8a7d97935cf9898407a90369e9637
rkdwlss2 <rkdwlss2@gmail.com>
2021-12-03 23:23:34 +0900

    boj5717

---
+++ BOJ5717.cpp
@@ -0,0 +1,19 @@
+//
+//  BOJ5717.cpp
+//  boj1926bfs
+//
+//  Created by 강명진 on 2021/12/03.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    while(true){
+        int a,b;
+        cin>>a>>b;
+        if (a==0&&b==0)break;
+        cout<<a+b<<'\n';
+        
+    }
+}
