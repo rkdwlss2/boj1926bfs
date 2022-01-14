040f8a004fdc389821a2ad54b2b020babc58ece9
rkdwlss2 <rkdwlss2@gmail.com>
2022-01-14 20:58:32 +0900

    boj14442

---
+++ boj14442.cpp
@@ -0,0 +1,83 @@
+//
+//  boj14442.cpp
+//  boj1926bfs
+//
+//  Created by 강명진 on 2022/01/14.
+//
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n,m,k;
+
+int board[1001][1001];
+int visited[1001][1001][11];
+int dx[4]={0,0,1,-1};
+int dy[4]={1,-1,0,0};
+void bfs(){
+    queue<pair<pair<int,int>,int>> q;
+    q.push({{0,0},0});
+    visited[0][0][0]=1;
+    while(!q.empty()){
+        auto now=q.front();q.pop();
+        int x=now.first.first;
+        int y=now.first.second;
+        int num=now.second;
+        for (int dir=0;dir<4;dir++){
+            int nx=x+dx[dir];
+            int ny=y+dy[dir];
+            if (nx<0||nx>=n||ny<0||ny>=m)continue;
+            if (num>=k){
+                if (board[nx][ny]==1||visited[nx][ny][num]>0)continue;
+                visited[nx][ny][num]=visited[x][y][num]+1;
+                q.push({{nx,ny},num});
+            }else{
+                if (visited[nx][ny][num]>0)continue;
+                if (board[nx][ny]==1){
+                    visited[nx][ny][num+1]=visited[x][y][num]+1;
+                    q.push({{nx,ny},num+1});
+                }else{
+                    visited[nx][ny][num]=visited[x][y][num]+1;
+                    q.push({{nx,ny},num});
+                }
+            }
+        }
+        
+
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m>>k;
+    for (int i = 0 ; i<n;i++){
+        for (int j = 0 ; j <m;j++){
+            char tmp;
+            cin>>tmp;
+            int tmpNum=tmp-'0';
+            board[i][j]=tmpNum;
+        }
+    }
+    bfs();
+//    for (int i = 0 ; i <n;i++){
+//        for (int j = 0 ; j <m;j++){
+//            cout<<visited[i][j]<<' ';
+//        }cout<<'\n';
+//    }
+    int answer=987654321;
+    for (int i = 0 ; i<11;i++){
+        if (visited[n-1][m-1][i]>0&&answer>visited[n-1][m-1][i])answer=visited[n-1][m-1][i];
+    }
+    if (answer==987654321){
+        cout<<-1<<'\n';
+    }else{
+        cout<<answer<<'\n';
+    }
+//    if (visited[n-1][m-1]>0){
+//        cout<<visited[n-1][m-1]<<'\n';
+//    }else{
+//        cout<<-1<<'\n';
+//    }
+}
--- boj9207.cpp
+++ boj9207.cpp
@@ -1,102 +1,102 @@
+////
+////  boj9207.cpp
+////  boj1926bfs
+////
+////  Created by 강명진 on 2022/01/13.
+////
 //
-//  boj9207.cpp
-//  boj1926bfs
+//#include <iostream>
+//#include <queue>
+//#include <unordered_map>
+//#include <cmath>
+//#include <algorithm>
+//using namespace std;
 //
-//  Created by 강명진 on 2022/01/13.
+//int board[5][9];
+//int pin[8][3];
+//int ans2;
+//int dx[4]={0,0,1,-1};
+//int dy[4]={1,-1,0,0};
+//unordered_map<int, int> um;
+//priority_queue<pair<int,int>> pq;
 //
-
-#include <iostream>
-#include <queue>
-#include <unordered_map>
-#include <cmath>
-#include <algorithm>
-using namespace std;
-
-int board[5][9];
-int pin[8][3];
-int ans2;
-int dx[4]={0,0,1,-1};
-int dy[4]={1,-1,0,0};
-unordered_map<int, int> um;
-priority_queue<pair<int,int>> pq;
-
-bool moveOk(int x,int y,int dir){
-    int nx=x+dx[dir];
-    int ny=y+dy[dir];
-    if (nx<0||nx>=5||ny<0||ny>=9)return false;
-    if (board[nx][ny]!=1)return false;
-    int nnx=nx+dx[dir];
-    int nny=ny+dy[dir];
-    if (nnx<0||nnx>=5||nny<0||nny>=9)return false;
-    if (board[nnx][nny]!=0)return false;
-    return true;
-}
-
-
-
-void go(int cnt){
-
-    for (int i = 0 ; i <5;i++){
-        for (int j = 0 ; j <9;j++){
-            if (board[i][j]==1){
-                int x=i;
-                int y=j;
-                for (int dir=0;dir<4;dir++){
-                    if (moveOk(x,y,dir)){
-                        int nx=x+dx[dir];
-                        int ny=y+dy[dir];
-                        int nnx=nx+dx[dir];
-                        int nny=ny+dy[dir];
-                        board[x][y]=0;
-                        board[nx][ny]=0;
-                        board[nnx][nny]=1;
-                        go(cnt+1);
-                        board[x][y]=1;
-                        board[nx][ny]=1;
-                        board[nnx][nny]=0;
-                    }
-                }
-            }
-        }
-//        cout<<'\n';
-    }
-
-    ans2=max(ans2,cnt);
-}
-
-int main(){
-    int tc;
-    cin>>tc;
-    while(tc--){
-        ans2=-1;
-        int index=0;
-        for (int i = 0 ; i <5;i++){
-            for (int j = 0 ; j<9;j++){
-                char tmp;
-                cin>>tmp;
-                if (tmp=='o'){
-                    pin[index][0]=i;
-                    pin[index][1]=j;
-                    pin[index][2]=1;
-                    index++;
-                    board[i][j]=1;
-                }else if (tmp=='#'){
-                    board[i][j]=2;
-                }
-            }
-        }
-        go(0);
-        cout<<index-ans2<<' '<<ans2<<'\n';
-        for (int i = 0 ; i<5;i++){
-            fill(board[i],board[i]+9,0);
-        }
-        for (int i = 0 ;i<8;i++){
-            fill(pin[i],pin[i]+3,0);
-        }
-        while(!pq.empty()){
-            pq.pop();
-        }
-        um.clear();
-    }
-    
-}
+//bool moveOk(int x,int y,int dir){
+//    int nx=x+dx[dir];
+//    int ny=y+dy[dir];
+//    if (nx<0||nx>=5||ny<0||ny>=9)return false;
+//    if (board[nx][ny]!=1)return false;
+//    int nnx=nx+dx[dir];
+//    int nny=ny+dy[dir];
+//    if (nnx<0||nnx>=5||nny<0||nny>=9)return false;
+//    if (board[nnx][nny]!=0)return false;
+//    return true;
+//}
+//
+//
+//
+//void go(int cnt){
+//
+//    for (int i = 0 ; i <5;i++){
+//        for (int j = 0 ; j <9;j++){
+//            if (board[i][j]==1){
+//                int x=i;
+//                int y=j;
+//                for (int dir=0;dir<4;dir++){
+//                    if (moveOk(x,y,dir)){
+//                        int nx=x+dx[dir];
+//                        int ny=y+dy[dir];
+//                        int nnx=nx+dx[dir];
+//                        int nny=ny+dy[dir];
+//                        board[x][y]=0;
+//                        board[nx][ny]=0;
+//                        board[nnx][nny]=1;
+//                        go(cnt+1);
+//                        board[x][y]=1;
+//                        board[nx][ny]=1;
+//                        board[nnx][nny]=0;
+//                    }
+//                }
+//            }
+//        }
+////        cout<<'\n';
+//    }
+//
+//    ans2=max(ans2,cnt);
+//}
+//
+//int main(){
+//    int tc;
+//    cin>>tc;
+//    while(tc--){
+//        ans2=-1;
+//        int index=0;
+//        for (int i = 0 ; i <5;i++){
+//            for (int j = 0 ; j<9;j++){
+//                char tmp;
+//                cin>>tmp;
+//                if (tmp=='o'){
+//                    pin[index][0]=i;
+//                    pin[index][1]=j;
+//                    pin[index][2]=1;
+//                    index++;
+//                    board[i][j]=1;
+//                }else if (tmp=='#'){
+//                    board[i][j]=2;
+//                }
+//            }
+//        }
+//        go(0);
+//        cout<<index-ans2<<' '<<ans2<<'\n';
+//        for (int i = 0 ; i<5;i++){
+//            fill(board[i],board[i]+9,0);
+//        }
+//        for (int i = 0 ;i<8;i++){
+//            fill(pin[i],pin[i]+3,0);
+//        }
+//        while(!pq.empty()){
+//            pq.pop();
+//        }
+//        um.clear();
+//    }
+//    
+//}
