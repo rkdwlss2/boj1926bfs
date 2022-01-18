96e6fde5a3b45ffee7d9ffab45967a01fbe17028
rkdwlss2 <rkdwlss2@gmail.com>
2022-01-19 01:10:59 +0900

    boj9944

--- boj1781.cpp
+++ boj1781.cpp
@@ -1,24 +1,24 @@
-#include<cstdio>
-#include<algorithm>
-#include<queue>
-using namespace std;
-
-#define mxn 200000
-pair<int,int> a[mxn];
-
-int main(){
-    int n, ans=0;
-    priority_queue<int> q;
-    scanf("%d",&n);
-    for( int i=0; i<n; i++ ) scanf("%d%d",&a[i].first,&a[i].second);
-    sort(a,a+n);
-    for( int i=n, j=n-1; i; i-- ){
-        while( j>=0 && a[j].first >= i ) q.push(a[j--].second);
-        if( !q.empty() ){
-            ans += q.top();
-            q.pop();
-        }
-    }
-    printf("%d",ans);
-    return 0;
-}
+//#include<cstdio>
+//#include<algorithm>
+//#include<queue>
+//using namespace std;
+//
+//#define mxn 200000
+//pair<int,int> a[mxn];
+//
+//int main(){
+//    int n, ans=0;
+//    priority_queue<int> q;
+//    scanf("%d",&n);
+//    for( int i=0; i<n; i++ ) scanf("%d%d",&a[i].first,&a[i].second);
+//    sort(a,a+n);
+//    for( int i=n, j=n-1; i; i-- ){
+//        while( j>=0 && a[j].first >= i ) q.push(a[j--].second);
+//        if( !q.empty() ){
+//            ans += q.top();
+//            q.pop();
+//        }
+//    }
+//    printf("%d",ans);
+//    return 0;
+//}
---
+++ boj9944.cpp
@@ -0,0 +1,126 @@
+//
+//  boj9944.cpp
+//  boj1926bfs
+//
+//  Created by 강명진 on 2022/01/18.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int n,m;
+int board[31][31];
+int answer=1000001;
+int dx[4]={0,0,1,-1};
+int dy[4]={1,-1,0,0};
+
+pair<int,pair<int,int>> Fill(int sx,int sy,int dir){
+    int x=sx;
+    int y=sy;
+    int num=0;
+    int nx,ny;
+        while(true){
+            nx=x+dx[dir];
+            ny=y+dy[dir];
+            if (nx<0||nx>=n||ny<0||ny>=m)break;
+            if (board[nx][ny]==1||board[nx][ny]>1)break;
+            board[nx][ny]=2;
+            x=nx;
+            y=ny;
+            num++;
+        }
+    return {num,{nx-dx[dir],ny-dy[dir]}};
+}
+
+void Delete(int sx,int sy,int dir,int num){
+    int x=sx;
+    int y=sy;
+        while(num>0){
+            int nx=x+dx[dir];
+            int ny=y+dy[dir];
+            if (nx<0||nx>=n||ny<0||ny>=m)continue;
+            board[nx][ny]=0;
+            x=nx;
+            y=ny;
+            num--;
+        }
+}
+
+void go(int x,int y,int cnt){
+    bool Ok=false;
+    for (int dir=0;dir<4;dir++){
+        int nx=x+dir[dx];
+        int ny=y+dir[dy];
+        if (nx<0||nx>=n||ny<0||ny>=m)continue;
+        if (board[nx][ny]==1||board[nx][ny]==2)continue;
+        Ok=true;
+        pair<int,pair<int,int>> now=Fill(x,y,dir);
+        int num=now.first;
+        int nextX=now.second.first;
+        int nextY=now.second.second;
+        go(nextX,nextY,cnt+1);
+        Delete(x,y,dir,num);
+    }
+    if (!Ok){
+        bool Ok2=false;
+        for (int i = 0 ; i <n;i++){
+            for (int j = 0 ; j <m;j++){
+                if (board[i][j]==0){
+                    Ok2=true;
+                    break;
+                }
+            }
+            if (Ok2)break;
+        }
+        if (!Ok2&&answer>cnt)answer=cnt;
+    }
+}
+
+int main(){
+    int index=1;
+    string str;
+    while (cin>>n>>m) {
+//        getline(cin,str);
+//        if (str=="")break;
+//        string tmpN,tmpM;
+//        bool space=false;
+//        for (int i = 0 ; i<str.size();i++){
+//            if (str[i]==' '){
+//                space=true;
+//                continue;
+//            }
+//            if (!space)tmpN+=str[i];
+//            else tmpM+=str[i];
+//        }
+//        n=atoi(tmpN.c_str());
+//        m=atoi(tmpM.c_str());
+        for (int i = 0 ; i <n;i++){
+            for (int j = 0 ; j <m;j++){
+                char tmp;
+                cin>>tmp;
+                if (tmp=='.'){
+                    board[i][j]=0;
+                }else{
+                    board[i][j]=1;
+                }
+            }
+        }
+        for (int i = 0 ; i < n;i++){
+            for (int j = 0 ; j <m;j++){
+                if (board[i][j]==0){
+                    board[i][j]=2;
+                    go(i,j,0);
+                    board[i][j]=0;
+                }
+            }
+        }
+
+        if (answer==1000001){
+            cout<<"Case "<<index<<": "<<-1<<'\n';
+        }else{
+            cout<<"Case "<<index<<": "<<answer<<'\n';
+        }
+        index++;
+        answer=1000001;
+    }
+}
